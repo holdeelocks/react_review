62715f032330bc6229115fe99529ef17dc0f9bd5
Holden B <holden.bucher.92@gmail.com>
2019-01-26 14:46:29 -0600

    added context

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { Switch } from '../switch';
 
+const ToggleContext = React.createContext();
+
 class Toggle extends React.Component {
+	static On = ({ children }) => (
+		<ToggleContext.Consumer>
+			{contextValue => (contextValue.on ? children : null)}
+		</ToggleContext.Consumer>
+	);
+	static Off = ({ children }) => (
+		<ToggleContext.Consumer>
+			{contextValue => (contextValue.on ? null : children)}
+		</ToggleContext.Consumer>
+	);
+	static Button = props => (
+		<ToggleContext.Consumer>
+			{contextValue => <Switch on={contextValue.on} onClick={contextValue.toggle} {...props} />}
+		</ToggleContext.Consumer>
+	);
+
 	state = {
 		on: false
 	};
@@ -15,12 +33,30 @@ class Toggle extends React.Component {
 		);
 	};
 	render() {
-		return <Switch on={this.state.on} />;
+		return (
+			<ToggleContext.Provider
+				value={{
+					on: this.state.on,
+					toggle: this.toggle
+				}}
+			>
+				{this.props.children}
+			</ToggleContext.Provider>
+		);
 	}
 }
 
 function Usage({ onToggle = (...args) => console.log('onToggle', ...args) }) {
-	return <Toggle onToggle={onToggle} />;
+	return (
+		<Toggle onToggle={onToggle}>
+			<Toggle.On>The button is on</Toggle.On>
+			<Toggle.Off>The button is off</Toggle.Off>
+			<div>
+				<Toggle.Button />
+			</div>
+		</Toggle>
+	);
 }
+Usage.title = 'Flexible Compound Components';
 
 export { Toggle, Usage as default };
